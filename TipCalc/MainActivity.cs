2ca318e8803183320059a3002e4a31760f91b32d
ViktorOnegin <viktor.onegin@tptlive.ee>
2018-09-26 18:13:46 +0300

    done
    done

---
+++ TipCalc/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace TipCalc
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView tip;
+        TextView tipAmount;
+        TextView totalAmount;
+        EditText billAmountValue;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var calculateBtn = FindViewById<Button>(Resource.Id.calculateBtn);
+            var seekBar1 = FindViewById<SeekBar>(Resource.Id.seekBar1);
+            tip = FindViewById<TextView>(Resource.Id.tip2);
+            tipAmount = FindViewById<TextView>(Resource.Id.tipAmount2);
+            totalAmount = FindViewById<TextView>(Resource.Id.totalAmount2);
+            billAmountValue = FindViewById<EditText>(Resource.Id.editText);
+
+            seekBar1.ProgressChanged += SeekBar1_ProgressChanged;
+            calculateBtn.Click += CalculateBtn_Click;
+        }
+
+        private void SeekBar1_ProgressChanged(object sender,SeekBar.ProgressChangedEventArgs e)
+        {
+            tip.Text = e.Progress.ToString();
+        }
+
+        private void CalculateBtn_Click(object sender, System.EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            //Tip Amount = bill amount * Tip / 100
+            //Total Amount = bill amount + Tip amount
+
+            float Tip = float.Parse(tip.Text) / 100;
+            tipAmount.Text = (int.Parse(billAmountValue.Text) * Tip).ToString();
+            totalAmount.Text = (int.Parse(billAmountValue.Text) + int.Parse(tipAmount.Text)).ToString();
+
+
+        }
+    }
+}
---
+++ TipCalc/obj/Debug/MonoAndroid81/android/src/md558ae09816f846753df232a9c9e8da0e0/MainActivity.java
@@ -0,0 +1,47 @@
+package md558ae09816f846753df232a9c9e8da0e0;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("TipCalc.MainActivity, TipCalc", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("TipCalc.MainActivity, TipCalc", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
